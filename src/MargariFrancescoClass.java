e76ff6d563d6aa55f6585f786d42279f79ceaeaf
MargariFrancesco <4ci@gmail>
2022-05-04 11:39:24 +0200

    versione 0:errata

---
+++ src/MargariFrancescoClass.java
@@ -0,0 +1,58 @@
+/**
+ * <h1><U>Calcolo sommatoria</U></h1> 
+ * <i>ditta meucci</i>
+ * <p><b>Margari Francesco</b></p>
+ * <u>4cI</u>
+ * <i>04/05/2022</i>
+ * 
+ * Funzioni:
+ * <li>calcola sommatoria
+ * 
+ * 
+ * @author francesco margari
+ * @version 1.0
+ * 
+ */
+public class MargariFrancescoClass {
+  
+	/**
+	 * @param s numero intero
+	 * @param f numero intero 
+	 */
+	private int s,f;
+
+	/**
+	 * costruttore
+	 * @param s numero intero
+	 * @param f numero intero
+	 */
+	public MargariFrancescoClass(int s, int f) {
+		this.s = s;
+		this.f = f;
+	}
+	
+	/**
+	 * 
+	 * @param s
+	 * @param f
+	 * @return sommatoria intero
+	 */
+	public int margariMet(int s,int f) {
+		int r=0;
+		for(int i=1;i<f;i++) {
+			r=r*i;
+		}
+		return r;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		MargariFrancescoClass f=new MargariFrancescoClass(4,3);
+		
+		f.margariMet(4,3);
+		f.margariMet(3,3);
+	}
+
+}
